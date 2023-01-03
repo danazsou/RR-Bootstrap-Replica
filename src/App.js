7fbe0fc673a2744563eef59a9a07d1d4dc68848f
danazou <dana.sou@gmail.com>
2023-01-03 00:04:59 -0800

    first commit with components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+     <topBar/>
+     <backGround/>
+     <storeListings/>
     </div>
   );
 }
